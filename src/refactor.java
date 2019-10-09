9e4739e481e4ee948fab3529d88361c690d2c8a9
NguyenVinh <nguyenquangvinh2518@gmail.com>
2019-10-09 16:42:32 +0700

    refactor

---
+++ src/refactor.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class refactor {
+    public static String fizzBuzz(int number){
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+
+        if(isFizz && isBuzz)
+            return "FizzBuzz";
+
+        if(isFizz)
+            return "Fizz";
+
+        if(isBuzz)
+            return "Buzz";
+
+        return number + "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(3));
+    }
+}
